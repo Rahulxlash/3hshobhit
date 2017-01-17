2db8e3decdb70cde4de73d1489c49083300f9f27
rahulxlash <rahulsharmpro@gmail.com>
2017-01-17 21:46:53 +0530

    Project Init

---
+++ 3HM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3HM.Startup))]
+namespace _3HM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
